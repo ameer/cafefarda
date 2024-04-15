689fdbb41381a62fa489de762c8978a2e01f14d6
Amir Mousavi <a.mousavi@arasdp.ir>
2024-04-15 08:54:50 +0330

    add branch card

---
+++ components/branchCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card>
+    <v-img
+      :src="image"
+      class="white--text align-end"
+      gradient="to bottom, rgba(0,0,0,.15), rgba(0,0,0,.8)"
+      height="200px"
+    >
+      <v-card-title class="pt-0">
+        {{ name }}
+      </v-card-title>
+      <v-card-subtitle>{{ address }}</v-card-subtitle>
+      <div class="px-4 pb-4 d-flex align-center">
+        <v-btn icon outlined dark :href="`tel:${phone}`">
+          <v-img contain src="/icons/phone.svg" max-width="20" height="20" />
+        </v-btn>
+        <v-btn icon outlined dark :href="location" class="mr-3">
+          <v-img contain src="/icons/directions.svg" max-width="20" height="20" />
+        </v-btn>
+        <v-spacer />
+
+        <v-btn class="rounded-lg" :to="{path: `/menu/${branch}`}" outlined dark>
+          <v-img class="mr-n2" contain src="/icons/list.svg" max-width="20" height="20" />
+          <span class="mr-2">منو</span>
+        </v-btn>
+      </div>
+    </v-img>
+    <!-- <v-container>
+      <v-row align="stretch">
+        <v-col cols="3">
+          <v-img src="/images/branches/mehrvila.jpg" />
+        </v-col>
+        <v-col />
+      </v-row>
+    </v-container> -->
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    address: {
+      type: String,
+      default: ''
+    },
+    phone: {
+      type: String,
+      default: ''
+    },
+    location: {
+      type: String,
+      default: ''
+    },
+    branch: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,23 @@
   <v-container fluid class="pa-0 h-100 fardaGreen-gr">
     <v-img src="/startup/5.jpg" position="left center" class="h-100">
       <v-container class="h-100">
-        <v-row align="center" justify="center" class="h-100">
+        <v-row align="start" justify="center" class="h-100">
           <v-col cols="12" md="6">
             <v-img
               src="/cafe-farda-logo.svg"
               contain
-              @click="counter"
+              max-width="80%"
+              class="mx-auto"
             />
-            <div v-if="show === 3" class="d-flex flex-column">
-              <v-btn v-for="(b, i) in branches" :key="`bb-${i}`" class="mb-2" outlined :to="{path: `/menu/${b.branch}`}">
+            <div class="d-flex flex-column mt-4">
+              <branch-card
+                v-for="(b, i) in branches"
+                v-bind="b"
+                :key="`bb-${i}`"
+                class="mb-4"
+              >
                 {{ b.name }}
-              </v-btn>
+              </branch-card>
             </div>
           </v-col>
         </v-row>
@@ -27,9 +33,9 @@ export default {
   data () {
     return {
       branches: [
-        { name: 'مهرویلا', branch: 'mehrvila' },
-        { name: 'گوهردشت', branch: 'gohardasht' },
-        { name: 'سه‌راه', branch: '3rah' }
+        { name: 'مهرویلا', address: 'مهرویلا، خیابان رودکی غربی، نبش تقاطع سعدی', image: '/images/branches/mehrvila.jpg', phone: '02633554014', location: 'https://www.google.com/maps/dir//Alborz+Province,+Karaj,+Mehrvila,+%D8%B3%D8%B9%D8%AF%DB%8C%D8%8C+RX84%2BMH9+Farda+Cafe%E2%80%AD/@35.8166714,50.9564745,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x3f8dbe5bdec79ed7:0xa055c210c994b2c7!2m2!1d50.956463!2d35.8166731!3e0?entry=ttu', branch: 'mehrvila' },
+        { name: 'گوهردشت', address: 'فاز دوم، خیابان دکتر حسابی شمالی، نبش نهم غربی', image: '/images/branches/gohardasht.jpg', phone: '02634421473', location: 'https://www.google.com/maps/dir//Cafe+Farda+(%DA%A9%D8%A7%D9%81%D9%87+%D9%81%D8%B1%D8%AF%D8%A7),+VX4F%2BGRC+Gohardasht,+Karaj,+Alborz+Province%E2%80%AD/@35.8562709,50.9745033,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x3f8dbf749a1aa2b9:0xe5ec4979a5d9086c!2m2!1d50.9744921!2d35.856289!3e0?entry=ttu', branch: 'gohardasht' },
+        { name: 'سه‌راه گوهردشت', address: 'کنار گذر پل سه راه گوهردشت، روبروی ایستگاه تاکسی، پایین‌تر از نانوایی اپامه', image: '/images/branches/3rah.jpg', phone: '09360767684', location: 'https://www.google.com/maps/dir//Cafe+Farda+(%DA%A9%D8%A7%D9%81%D9%87+%D8%B1%D8%B3%D8%AA%D8%B1%DB%8C+%D9%81%D8%B1%D8%AF%D8%A7),+RXH9%2BC67+South+Kuye+Karmandan,+Karaj,+Alborz+Province%E2%80%AD/@35.8285375,50.9680469,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x3f8dbfbb70e65b0f:0x3163a85055477d6!2m2!1d50.9681133!2d35.8285117!3e0?entry=ttu', branch: '3rah' }
       ],
       show: 0
     }
